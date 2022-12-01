24f66730cab710d190ad132a57dc9810b54355cd
Ivan Seibel <me@ivanseibel.dev>
2022-12-01 22:07:40 +0000

    creates new Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,35 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar} 
+            src="https://github.com/ivanseibel.png" 
+            alt="Author avatar" 
+          />
+          <div className={styles.authorInfo}>
+            <strong>Ivan Seibel</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='First of December at 21:24' dateTime='2022-12-01 21:24'>Published 1h ago</time>
+      </header>
+      <div className={styles.content}>
+      <p>Hey guys 👋</p>
+
+      <p>I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. Project name is DoctorCare 🚀</p>
+
+      <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
+
+      <p>
+        <a href='#'>#newproject</a>{' '}
+        <a href='#'>#nlw</a>{' '}
+        <a href='#'>#rocketseat</a>
+      </p>
+      </div>
+    </article>
+  )
+}
