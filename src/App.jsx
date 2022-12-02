c2df22fd4e9eae082a733c33e6292639669ce41a
Ivan Seibel <me@ivanseibel.dev>
2022-12-02 23:00:42 +0000

    rendering posts dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,57 @@ import { Sidebar } from './components/Sidebar'
 import './global.css'
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ivanseibel.png',
+      name: 'Ivan Seibel',
+      role: 'Software Engineer',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Hey guys 👋',
+      },
+      {
+        type: 'paragraph',
+        content: "I just uploaded another project to my portfolio. It's a experiment with Vitejs and Reactjs 🚀",
+      },
+      {
+        type: 'link',
+        content: 'ivanseibel.dev/vitejs-reactjs',
+        href: '#',
+      },
+    ],
+    publishedAt: new Date('2022-12-01 21:24:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego',
+      role: 'CTO Rocketseat',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Hey guys 👋',
+      },
+      {
+        type: 'paragraph',
+        content: "I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. Project name is DoctorCare 🚀",
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare',
+        href: '#',
+      },
+    ],
+    publishedAt: new Date('2022-12-02 03:00:00'),
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -13,14 +64,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Ivan Luis Seibel"
-            content="Hello World"
-          />
-          <Post 
-            author="John Doe"
-            content="Laborum sit nulla voluptate ipsum occaecat mollit veniam ipsum exercitation laborum do cillum magna."
-          />
+          {posts.map((post) => (
+            <Post 
+              key={post.id} 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt} 
+            />
+          ))}
         </main>
       </div>
     </div>
