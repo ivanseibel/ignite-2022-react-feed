b744325a3fe118d1ce449ae53632c0add70ff527
Ivan Seibel <me@ivanseibel.dev>
2022-12-03 01:36:39 +0000

    add feature to delete comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { HandsClapping, ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ id, content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(id);
+  }
+  
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/ivanseibel.png" alt="Comment author avatar" />
@@ -15,7 +19,10 @@ export function Comment({ content }) {
               <time title='First of December at 21:24' dateTime='2022-12-01 21:24'>About 1h ago</time>
             </div>          
             
-            <button title='Delete comment'>
+            <button 
+              title='Delete comment'
+              onClick={handleDeleteComment}
+            >
               <Trash size={24} />
             </button>
           </header>
